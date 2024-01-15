2f0e6d3eb29a2f35fb2766eb3d5e2fa784c3fb00
Italo Alencar <italo-alencar2000@hotmail.com>
2024-01-15 18:40:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+  /* const image = html.querySelector("#profile img");
+  if (html.classList.contains("light")) {
+    image.setAttribute("src", "assets/3106921.png");
+  } else {
+    image.setAttribute("src", "assets/5987462.png");
+  } */
+}
